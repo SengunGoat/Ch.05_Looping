7c8c711dc4fb41cc85376fae3f149f553a77c8d3
toricden <toricden@urbandaleschools.com>
2021-10-05 10:45:52 -0500

    Save Catorce

--- 5.1_Coin_Toss.py
+++ 5.1_Coin_Toss.py
@@ -6,8 +6,22 @@ COIN TOSS PROGRAM
 3.) Add a loop so that the program does this 50 times.
 4.) Create a running total for the number of heads and the number of tails and print the total at the end.
 '''
+import random
+heads=0
+tails=0
 
-
+for i in range(50):
+    onetwo = random.randint(0, 1)
+    if onetwo==1:
+        print("You got heads!")
+        heads+=1
+    else:
+        print("You got tails!")
+        tails+=1
+print()
+print("You got heads",heads,"times!")
+print()
+print("You got tails",tails,"times!")
 
 
 
